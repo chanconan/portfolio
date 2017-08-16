df3d5d0a28cc64f332f46dcfd3ab7da3a9a04e93
Conan Chan <chanconan67@gmail.com>
2017-08-16 12:00:10 -0700

    Moved script to JS file and updated website

---
+++ static/js/website.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+    // Initialize parallax
+    $('.parallax').parallax();
+    // Navbar links will scroll webpage to the correct div
+    $('#navlink-aboutMe').click(function(){
+        $('html, body').animate({
+            scrollTop: $("#aboutMe").offset().top - 80
+        }, 2000);
+    });
+    $('#navlink-skills').click(function(){
+        $('html, body').animate({
+            scrollTop: $("#skills").offset().top - 80
+        }, 2000);
+    });
+    $('#navlink-projects').click(function(){
+        $('html, body').animate({
+            scrollTop: $("#projects").offset().top - 80
+        }, 2000);
+    });
+    $('#navlink-contact').click(function(){
+        $('html, body').animate({
+            scrollTop: $("#contactMe").offset().top - 80
+        }, 2000);
+    });
+
+});
